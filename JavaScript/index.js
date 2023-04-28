33843e254616eae329fa16bacdd0f6c51981d5ef
unknown <abatantuono1712@gmail.com>
2023-04-28 00:07:05 -0300

    menuMobileOffcanvas

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -3,3 +3,13 @@ window.addEventListener('scroll', function () {
     navbar.classList.toggle('scrolled', window.scrollY > 550);
 });
 
+
+
+const menuMobile = document.querySelector('.menuMobile');
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 700) {
+      menuMobile.classList.add('menuMobile-scroll');
+    } else {
+      menuMobile.classList.remove('menuMobile-scroll');
+    }
+  });
